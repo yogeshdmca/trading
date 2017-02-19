2168ae84c338a6695216f10ebce11c6e23a485c7
Yogesh <alenwork@hotmail.com>
2017-02-19 13:40:47 +0530

    commit for api for add trade

--- trade/urls.py
+++ trade/urls.py
@@ -2,10 +2,11 @@
 
 from django.conf.urls import url, include
 
-from .views import (AccountBalanceReportUpdate, )
+from .views import (AccountBalanceReportUpdate,ApiCreateTrade )
 from .ajax import *
 
 urlpatterns = [
+	url(r'^api/create/trade/$', ApiCreateTrade.as_view(), name='api-create-trade'),
     url(r'^accounts/balance/report/$', AccountBalanceReportUpdate.as_view(), name='accounts-balance-report'),
     url(r'^accounts/subscribe/autotrade/$', subscribe_autotrade, name='accounts-subscribe-autotrade'),
     ]
--- trade/views.py
+++ trade/views.py
@@ -4,12 +4,14 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic.list import ListView
-from .models import UserBalanceInfo
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from .models import UserBalanceInfo, Currency, ExpireIN, Signal
 from accounts.models import Profile
 from django.utils import timezone
 from utils import update_balance_by_socket
+import json
 
 # Create your views here.
 class AccountBalanceReportUpdate(LoginRequiredMixin,ListView):
@@ -27,3 +29,38 @@ class AccountBalanceReportUpdate(LoginRequiredMixin,ListView):
                 update_balance_by_socket(profile, UserBalanceInfo)
 
         return HttpResponseRedirect(reverse('accounts-balance-report'))
+
+class ApiCreateTrade(View):
+
+    #@method_decorator(csrf_exempt)
+    def get(self, request):
+        vals = request.GET
+        if vals.get('token') != '9926011586':
+            return HttpResponse(status=404)
+        currency_code = vals.get('currency_code', None)
+        currency_name = vals.get('currency_name', None)
+        time = vals.get('expire_time')
+        unit = vals.get('expire_unit')
+        direction = vals.get('direction')
+        responce={'status':200}
+
+        currency = currency_code and Currency.objects.get_or_create(pair_name=currency_code, name=currency_name)[0] or False
+        expire_in = (time and unit) and ExpireIN.objects.get_or_create(unit=unit, time=time)[0] or False
+        direction = direction in ['up','down'] and direction  or False
+
+        if not currency:
+            responce.update({'currency pair': 'invalid currency pair'})
+
+        elif not expire_in:
+            responce.update({'expire_in': 'invalid expire_in'})
+
+        elif not direction:
+            responce.update({'direction': 'invalid direction'})
+        else:
+
+            Signal.objects.create(currency=currency, expire_in=expire_in, direction=direction,user_id=1)
+            responce.update({'saved': 'OK','autotrade':'procesing'})
+        return HttpResponse(json.dumps(responce))
+
+
+
